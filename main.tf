12d81e6d3a4d1667265013e1e1f294715f1dc43b
suchitakanekar2-design <suchitakanekar2@gmail.com>
2025-10-20 09:07:51 +0530

    Change azurerm provider version and modify backend
    Updated azurerm provider version and commented out backend configuration.

--- main.tf
+++ main.tf
@@ -2,21 +2,22 @@ terraform {
  required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "~>3.0"
+        version = "4.48.0"
     } 
  } 
 
  backend "azurerm" {
-  azurerm_resource_group = "dev-rg"
-  azurerm_storage_account = "devstorage980"
-  container_name = "devstoragecontainer980"
-  key = "dev.tfstate"
+  # azurerm_resource_group = "dev-rg"
+  # azurerm_storage_account = "devstorage980"
+  # container_name = "devstoragecontainer980"
+  # key = "dev.tfstate"
    
  }
 }
 
 provider "azurerm" {
     features {}
+subscriptions_id = "9f14d6da-b117-434d-922f-f763c8ec88bb"
   
 }
 
